e50aa14a31578e73e7b034b8339c89a0dc66b129
Suresh Kotha <suresh.kotha@gmail.com>
2021-09-21 19:57:23 +0530

    changes

--- patterns/structural/bridge/TestBridge.java
+++ patterns/structural/bridge/TestBridge.java
@@ -7,5 +7,11 @@ public class TestBridge {
 
         view = new ShortView(new Book("DS", "This is a Data Structure Book", "www.amazon.com" ,"c:/test.jpg"));
         view.renderView();
+
+        view = new LongView(new ElectronicDevice("S7 Edge", "This is a smart phone by Samsung", "www.samsung.com" ,"c:/s7edge.jpg"));
+        view.renderView();
+
+        view = new ShortView(new ElectronicDevice("S7 Edge", "This is a smart phone by Samsung", "www.samsung.com" ,"c:/s7edge.jpg"));
+        view.renderView();
     }
 }
