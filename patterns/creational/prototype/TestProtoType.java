3fa081c73fa3f0330db11930d10e53a83e6cbac1
Suresh Kotha <suresh.kotha@gmail.com>
2021-09-22 02:38:04 +0530

    changes

--- patterns/creational/prototype/TestProtoType.java
+++ patterns/creational/prototype/TestProtoType.java
@@ -4,8 +4,10 @@ public class TestProtoType {
     public static void main(String[] args) {
         SongLibrary sl = new SongLibrary("My Favorites");
         System.out.println(sl);
+        System.out.println(System.identityHashCode(sl));
         SongLibrary sl2 = (SongLibrary) CloneFactory.getInstance().makeCopy(sl);
         sl2.setName("New Favourites");
         System.out.println(sl2);
+        System.out.println(System.identityHashCode(sl2));
     }
 }
